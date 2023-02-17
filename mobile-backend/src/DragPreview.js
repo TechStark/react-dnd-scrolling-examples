e4d2faa0580645d5359edeb128411443fea1b5ed
Wilson Tian <wilson.tian@perkinelmer.com>
2023-02-17 22:20:37 +0800

    update mobile example

--- mobile-backend/src/DragPreview.js
+++ mobile-backend/src/DragPreview.js
@@ -1,29 +1,29 @@
-import React, { PureComponent } from 'react';
-import { DragLayer } from 'react-dnd';
-import './DragPreview.css';
+import React from "react";
+import { useDragLayer } from "react-dnd";
+import "./DragPreview.css";
 
-class DragPreview extends PureComponent {
-  render() {
-    const { item, offset } = this.props;
+const DragPreview = () => {
+  const { item, offset } = useDragLayer((monitor) => ({
+    item: monitor.getItem(),
+    offset: monitor.getClientOffset(),
+  }));
 
-    return (
-      <div className="DragPreview">
-        {item && (
-          <div
-            className="DragPreview__item"
-            style={{
-              position: 'absolute',
-              top: offset.y,
-              left: offset.x
-            }}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="DragPreview">
+      {item && (
+        <div
+          className="DragPreview__item"
+          style={{
+            position: "absolute",
+            top: offset.y,
+            left: offset.x,
+          }}
+        >
+          {item.label}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default DragLayer(monitor => ({
-  item: monitor.getItem(),
-  offset: monitor.getClientOffset()
-}))(DragPreview);
+export default DragPreview;
